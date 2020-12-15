e525522aab3d4847e6e464fbd892335a2b4898da
Sherif Ahmed <stormyshadow2009@outlook.com>
2020-12-15 21:46:10 +0200

    .........

--- src/header/logo.js
+++ src/header/logo.js
@@ -2,7 +2,7 @@
 const Logo =()=>{
     return(
         <div className="col my-2">
-            <a href="#" data-toggle="tooltip" title="Future Style">
+            <a href="#" data-toggle="tooltip" title="Anwar Ahmed">
             <img src='../images/logo.jpg' className="rounded " 
             alt="logo" height="60px" width="60px" />
             </a>
